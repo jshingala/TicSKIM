a411f2f64b5a4d85749566f95f77d9246282cf41
CJ Pallari <cjpallari@gmail.com>
2025-05-25 03:01:26 -0700

    Refactor project structure, integrate Biztoc API, and update ticker handling
    - Added Biztoc API wrapper class to replace reliance on web scraping.
    - Updated spot.py: changed watchlist from list of tickers to dict mapping tickers to company names.
    - Introduced new folder structure for better organization.
    - Will refactor historical.py in a future commit to phase out Yahoo scraping in favor of Biztoc.

---
+++ biztoc/biztoc.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from config import BIZTOC_API_KEY
+
+
+class BIZTOC:
+    def __init__(self):
+        self.base_url = "https://biztoc.p.rapidapi.com"
+        self.headers = {
+            "x-rapidapi-key": BIZTOC_API_KEY,
+            "x-rapidapi-host": "biztoc.p.rapidapi.com",
+        }
+
+    def search_ticker(self, ticker):
+        url = f"{self.base_url}/search"
+        querystring = {"q": ticker}
+        response = requests.get(url, headers=self.headers, params=querystring)
+        formatted_data = json.dumps(response.json(), indent=5)
+        return formatted_data
+
+    def latest(self):
+        url = f"{self.base_url}/news/latest"
+        response = requests.get(url, headers=self.headers)
+        formatted_data = json.dumps(response.json(), indent=5)
+        return formatted_data
+
+    def search_source(self, source):
+        url = f"{self.base_url}/news/source/{source}"
+        response = requests.get(url, headers=self.headers)
+        formatted_data = json.dumps(response.json(), indent=5)
+        return formatted_data
+
+    def list_sources(self):
+        url = f"{self.base_url}/sources"
+        response = requests.get(url, headers=self.headers)
+        formatted_data = json.dumps(response.json(), indent=5)
+        return formatted_data
